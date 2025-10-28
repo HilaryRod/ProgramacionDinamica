01c7fa873755941c5843a46ec9e0d1654d80621e
HilaryRod <hilaryvirginia49@gmail.com>
2025-10-27 21:41:10 -0600

    Funcion para ver si es ganador o no

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -9,6 +9,7 @@ char tablero[9] = {'1','2','3','4','5','6','7','8','9'}; // Tablero para jugar 9
 
 //Declaracion de mis funciones
 void estructuragato();
+bool ganador();//declaracion para que sea falso o verdadero si gano o no
 
 //Funcion principal
 int main() {
@@ -16,7 +17,7 @@ int main() {
     cin >> jugador1;
     cout << "Ingresa el nombre del segundo jugador (O): ";
     cin >> jugador2;
-    estructuragato();//prueba para ver si funciona la estructura
+   
 }
 
 void estructuragato(){
@@ -25,4 +26,20 @@ void estructuragato(){
      cout << "  " << tablero[3] << " | " << tablero[4] << " | " << tablero[5] << endl;
      cout << " ---|---|---" << endl;
      cout << "  " << tablero[6] << " | " << tablero[7] << " | " << tablero[8] << endl;
+}
+
+bool ganador() {
+    int combinaciones[8][3] = { //va a recorrer cada una de las 8 combinaciones ganadoras
+        {0,1,2}, {3,4,5}, {6,7,8}, // Se puede ganar asi por filas
+        {0,3,6}, {1,4,7}, {2,5,8}, // Se puede ganar asi por columnas
+        {0,4,8}, {2,4,6}           // Se puede ganar asi en diagonales
+    };
+
+    for (auto &linea : combinaciones) { //para cada linea en combinacion, bucle
+
+        if (tablero[linea[0]] == turno && tablero[linea[1]] == turno && tablero[linea[2]] == turno) { //verifica si las 3 posiciones estan ocupadas por el simbolo del jugador
+            return true; //si es cierto el jugador ya gano y por eso devuelve true
+        }
+    }
+    return false; //si no retorna falso 
 }
