6649deb51aa8a573803942d09f30f5e95a3f6aad
HilaryRod <hilaryvirginia49@gmail.com>
2025-10-27 21:08:58 -0600

    Creacion estructura tablero gato

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,10 +7,22 @@ string jugador2;
 char turno = 'X';
 char tablero[9] = {'1','2','3','4','5','6','7','8','9'}; // Tablero para jugar 9 posiciones
 
+//Declaracion de mis funciones
+void estructuragato();
+
 //Funcion principal
 int main() {
     cout << "Ingresa el nombre del primer jugador (X): ";
     cin >> jugador1;
     cout << "Ingresa el nombre del segundo jugador (O): ";
     cin >> jugador2;
+    estructuragato();//prueba para ver si funciona la estructura
+}
+
+void estructuragato(){
+     cout << "\n  " << tablero[0] << " | " << tablero[1] << " | " << tablero[2] << endl;
+     cout << " ---|---|---" << endl;
+     cout << "  " << tablero[3] << " | " << tablero[4] << " | " << tablero[5] << endl;
+     cout << " ---|---|---" << endl;
+     cout << "  " << tablero[6] << " | " << tablero[7] << " | " << tablero[8] << endl;
 }
