d6af0e6b235ea7a68388b69b02f100c1d472fb86
HilaryRod <hilaryvirginia49@gmail.com>
2025-11-03 23:28:38 -0600

    Inicializacion tablero

--- conecta4.cpp
+++ conecta4.cpp
@@ -4,3 +4,12 @@ using namespace std;//para no tener que poner std en cada cin o cout
 //Se necesita hacer diferente al gato porque tiene mas casillas
 const int filas = 6; //defino num de filas
 const int columnas = 7;//defino num de columnas
+
+// Función para inicializar el tablero
+void inicializarTablero(char tablero[filas][columnas]) {
+    for (int i = 0; i < filas; i++) { //se recorren las filas
+        for (int k = 0; k < columnas; k++) { //se recorren las columnas
+            tablero[i][k] = ' ';
+        }
+    }
+}
