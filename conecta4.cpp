71c89e6bc53a22665dfed7532d53fb963164b70f
HilaryRod <hilaryvirginia49@gmail.com>
2025-11-03 23:27:25 -0600

    Creacion bibliotecas y primeras variables

--- conecta4.cpp
+++ conecta4.cpp
@@ -1,15 +1,6 @@
-#include <iostream> // Para entrada y salida
-using namespace std; //para no tener que usar std en todas las lineas
-
-//Declaracion de variables que voy a usar
-string jugador1;
-string jugador2;
-char turno = 'X';
-char tablero[14] = {'1','2','3','4','5','6','7','8','9','10','11','12','13','14'}; // Tablero para jugar 9 posiciones
-
-//Declaracion de mis funciones
-void estructurtablero();
-bool ganador();//declaracion para que sea falso o verdadero si gano o no
-void cambiarTurno();
-void jugar();
-void reiniciarTablero(); //funcion para reiniciar tablero o se limpiarlo
+//bIBLIOTECAS QUE SE USARAN
+#include <iostream>
+using namespace std;//para no tener que poner std en cada cin o cout 
+//Se necesita hacer diferente al gato porque tiene mas casillas
+const int filas = 6; //defino num de filas
+const int columnas = 7;//defino num de columnas
